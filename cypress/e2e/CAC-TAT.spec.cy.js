5530737ddf22d5503b24e61bbbb48eb95ed8bba9
leticiaocr <leticiabritooliveirac@gmail.com>
2024-06-27 18:05:07 -0300

    feat: acessando outra página

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -144,5 +144,23 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   })
 
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+    cy.get('#privacy a').should('have.attr', 'target', '_blank')
+  })
+
+  it('acessa a página da política de privacidade removendo o target e então clicanco no link', function() {
+    cy.get('#privacy a')
+      .invoke('removeAttr', 'target')
+      .click()
+
+    cy.contains('Talking About Testing').should('be.visible')
+  })
+
+  it.only('testa a página da política de privavidade de forma independente', function() {
+    cy.visit('./src/privacy.html')
+  
+    cy.contains('Talking About Testing').should('be.visible')
+  })
+  
 })
 
